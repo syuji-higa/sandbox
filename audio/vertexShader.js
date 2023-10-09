32190ec8a7b1cc77f8f8b5a51510acf41f1c73fd
HIGA Syuji <syuji.higa@gmail.com>
2023-10-09 21:21:27 +0000

    upadte

--- audio/vertexShader.js
+++ audio/vertexShader.js
@@ -8,10 +8,10 @@ uniform float sampleRate;
 out float sound;
 
 void main(void){
-  float frame = float(gl_VertexID);
+  float frame = float(gl_VertexID); // 頂点データのインデックスをフレーム数として使う。
   float time = frame / sampleRate; // 経過した秒数。
   float baseRate = TAU * time; // 周波数を 1Hz としたときの進行度（ラジアン）。
-  
+
   float volume = 0.5;
   float hz = 440.0;
 
